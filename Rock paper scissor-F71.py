b650dda48ffe1a9e39477c8ee04e7a5e38d985ce
Fabius716 <33468311+Fabius716@users.noreply.github.com>
2019-02-06 20:07:55 +0100

    Added an error system

--- Rock paper scissor-F71.py
+++ Rock paper scissor-F71.py
@@ -1,30 +1,33 @@
-
 import numpy.random as nr
-i=input("play\n")
-n=nr.randint(3)
-g=["r","p","s"]
-"""
-print(n)
-"""
-print(g[n])
+while True:
+	i=input("\nplay\n")
+	
+	n=nr.randint(3)
+	g=["r","p","s"]
+	
+	if (i!="s") and (i!="p") and (i!="r"):
+		print("Error")
+		continue
 
-if i == "r" and n==2:
-	print("win")
-if i == "r" and n==1:
-	print("loss")
-if i == "r" and n==0:
-	print("tie")
+	print(g[n])
 
-if i == "p" and n==2:
-	print("loss")
-if i == "p" and n==1:
-	print("tie")
-if i == "p" and n==0:
-	print("win")
+	if i == "r" and n==2:
+		print("win")
+	if i == "r" and n==1:
+		print("loss")
+	if i == "r" and n==0:
+		print("tie")
 
-if i == "s" and n==2:
-	print("tie")
-if i == "s" and n==1:
-	print("win")
-if i == "s" and n==0:
-	print("loss")
+	if i == "p" and n==2:
+		print("loss")
+	if i == "p" and n==1:
+		print("tie")
+	if i == "p" and n==0:
+		print("win")
+
+	if i == "s" and n==2:
+		print("tie")
+	if i == "s" and n==1:
+		print("win")
+	if i == "s" and n==0:
+		print("loss")
